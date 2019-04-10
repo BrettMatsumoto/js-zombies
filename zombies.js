0b2e99d2f2ad4063a9b518f0edc4f51d7b4e6983
Brett <brett_matsumoto@yahoo.com>
2019-04-10 09:57:59 -1000

    added FastZombies

--- zombies.js
+++ zombies.js
@@ -333,6 +333,11 @@ class Zombie {
  * @param {number} speed            The zombie's speed.
  */
 
+ class FastZombie extends Zombie {
+   constructor(health,strength,speed){
+     super(health,strength,speed);
+   }
+ }
 
 /**
  * FastZombie Extends Zombie Class
